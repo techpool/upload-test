d81d78fa3b9eb9e05cb557bac016fc828dbf41ef
techpool <suryadeep10@gmail.com>
2016-12-27 07:31:43 +0530

    initial and final commit

---
+++ routes/upload.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var router = express.Router();
+
+
+/* GET users listing. */
+router.post('/', function(req, res, next) {
+
+	console.log(req)
+
+    var username = req.body.name;
+    res.send({
+    	username: username,
+    	image: req.file.buffer.toString('base64')
+    })
+});
+
+module.exports = router;
